38ba9c323c72046a0cba13b9b082f0fe44a33314
Daniel <dsmeyer@avans.nl>
2017-05-18 14:44:06 +0200

    fragments working

--- app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
+++ app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
@@ -9,25 +9,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import layout.BlankFragment;
 import layout.SecondFragment;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
+    Fragment f1 = new BlankFragment();
+    Fragment f2 = new SecondFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(findViewById(R.id.main_framelayout) != null && savedInstanceState == null) {
+            BlankFragment blankFragment = new BlankFragment();
+            blankFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.main_framelayout, blankFragment, "blank").commit();
+        }
+
         Button btnChangeFragment = (Button) findViewById(R.id.ma_btn_switch);
         btnChangeFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Fragment fragment = new SecondFragment();
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.ma_fragment, fragment);
-                ft.commit();
+//                Fragment fragment = new SecondFragment();
+//                FragmentManager fm = getSupportFragmentManager();
+//                FragmentTransaction ft = fm.beginTransaction();
+//                ft.replace(R.id.ma_fragment, fragment);
+//                ft.commit();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                Fragment secondFragment = fragmentManager.findFragmentByTag("second");
+
+                if (secondFragment == null) {
+                    transaction.replace(R.id.main_framelayout, new SecondFragment(), "second");
+                    transaction.addToBackStack(null);
+                }
+                else {
+                    transaction.replace(R.id.main_framelayout, secondFragment, "second");
+                    transaction.addToBackStack(null);
+                }
+
+                transaction.commit();
             }
         });
     }
