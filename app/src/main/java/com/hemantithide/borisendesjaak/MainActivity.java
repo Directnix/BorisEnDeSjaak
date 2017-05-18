7012f61a4a3b62b9f4f31752d2bd3b36a348e5eb
Nick van Endhoven <nick_vend95@hotmail.com>
2017-05-18 12:24:46 +0200

    Changed fragment

--- app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
+++ app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
@@ -1,13 +1,34 @@
 package com.hemantithide.borisendesjaak;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import layout.SecondFragment;
+
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnChangeFragment = (Button) findViewById(R.id.ma_btn_switch);
+        btnChangeFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new SecondFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.ma_fragment, fragment);
+                ft.commit();
+            }
+        });
     }
 }
