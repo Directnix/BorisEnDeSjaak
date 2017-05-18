520a42946112f4725993f690367cfe51b36c4157
ThijsLaptop <thijs.wijnen@gmail.com>
2017-05-18 11:28:32 +0200

    working moving background

--- app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
+++ app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
@@ -8,17 +8,37 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    final ImageView backgroundOne = (ImageView)findViewById(R.id.main_imgvw_backgroundOne);
-    final ImageView backgroundTwo = (ImageView)findViewById(R.id.main_imgvw_backgroundTwo);
-    final ValueAnimator animator = ValueAnimator.ofFloat(0.0f ,1.0f);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //creating variables
+        final ImageView backgroundOne = (ImageView)findViewById(R.id.main_imgvw_backgroundOne);
+        final ImageView backgroundTwo = (ImageView)findViewById(R.id.main_imgvw_backgroundTwo);
+        final ValueAnimator animator = ValueAnimator.ofFloat(1.0f ,0.0f);
+        //setting animator up
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(100000L);
+        //actual method
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+        {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation)
+            {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
+        
+
 
 
     }
