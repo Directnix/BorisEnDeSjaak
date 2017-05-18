8257d54fe95979ce0e58614971e572b6422ecb2b
ThijsLaptop <thijs.wijnen@gmail.com>
2017-05-18 11:16:54 +0200

    working on moving background

--- app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
+++ app/src/main/java/com/hemantithide/borisendesjaak/MainActivity.java
@@ -1,13 +1,25 @@
 package com.hemantithide.borisendesjaak;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final ImageView backgroundOne = (ImageView)findViewById(R.id.main_imgvw_backgroundOne);
+    final ImageView backgroundTwo = (ImageView)findViewById(R.id.main_imgvw_backgroundTwo);
+    final ValueAnimator animator = ValueAnimator.ofFloat(0.0f ,1.0f);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+
+
     }
 }
