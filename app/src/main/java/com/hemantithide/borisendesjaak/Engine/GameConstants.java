b2f915a7851e900b69c6ec1c3740f7c971e9fdae
ThijsLaptop <thijs.wijnen@gmail.com>
2017-06-08 16:24:03 +0200

    2 new constants

--- app/src/main/java/com/hemantithide/borisendesjaak/Engine/GameConstants.java
+++ app/src/main/java/com/hemantithide/borisendesjaak/Engine/GameConstants.java
@@ -11,4 +11,7 @@ public class GameConstants {
     public final static int SWIPE_SPEED_HORIZONTAL = 20;
     public final static int SWIPE_SPEED_VERTICAL = 15;
     public final static int GAME_SPEED_INCREASE_INTERVAL = 140;
+
+    public final static int DRAGON_FIREBALL_INTERVAL = 60;
+    public final static int ANIMATION_SPEED = 10;
 }
--- app/src/main/java/com/hemantithide/borisendesjaak/GameObjects/Dragon.java
+++ app/src/main/java/com/hemantithide/borisendesjaak/GameObjects/Dragon.java
@@ -3,6 +3,7 @@ package com.hemantithide.borisendesjaak.GameObjects;
 import android.graphics.Canvas;
 import android.util.Log;
 
+import com.hemantithide.borisendesjaak.Engine.GameConstants;
 import com.hemantithide.borisendesjaak.GameActivity;
 import com.hemantithide.borisendesjaak.Engine.GameSurfaceView;
 import com.hemantithide.borisendesjaak.Engine.SpriteLibrary;
@@ -83,7 +84,7 @@ public class Dragon extends GameObject {
         if (state == PRESENT && !initFinished && posY == targetY) {
             game.activity.playSound(GameActivity.Sound.AYO_WHADDUP);
             initFinished = true;
-            fireballCooldown = 42;
+            fireballCooldown = GameConstants.DRAGON_FIREBALL_INTERVAL;
         }
 
         if (state == PRESENT && posY == targetY && posX == targetX && fireballCooldown == 0 && !game.activeStates.contains(GameSurfaceView.GameState.END_GAME)) {
@@ -138,7 +139,7 @@ public class Dragon extends GameObject {
             new Fireball(game, 2, 1);
             new Fireball(game, 3, 1);
             new Fireball(game, 4, 1);
-            fireballCooldown = 42;
+            fireballCooldown = GameConstants.DRAGON_FIREBALL_INTERVAL;
             game.activity.playSound(GameActivity.Sound.FIREBALL);
 
             firewaveCharge = 0;
@@ -150,7 +151,7 @@ public class Dragon extends GameObject {
         new Fireball(game, targetLaneX, game.speedMultiplier);
         targetLaneX = game.seed.fireballSeq.get(game.spawnWaveCount);
         targetX = game.laneXValues.get(targetLaneX);
-        fireballCooldown = 42;
+        fireballCooldown = GameConstants.DRAGON_FIREBALL_INTERVAL;
         game.activity.playSound(GameActivity.Sound.FIREBALL);
     }
 
